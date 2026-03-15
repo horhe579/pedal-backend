fa970f36bca5cd12064295d696119a4734df0d5f
Georgi Traykov <georgi.i.traykov@gmail.com>
2026-03-15 22:57:21 +0200

    feat: add JwtSettings model for configuration binding

--- Pedal/Models/JwtSettings.cs
+++ Pedal/Models/JwtSettings.cs
@@ -5,6 +5,6 @@ namespace Pedal.Models
         public required string SecretKey { get; set; }
         public required string Issuer { get; set; }
         public required string Audience { get; set; }
-        public int ExpirationMinutes { get; set; }
+        public required int ExpirationMinutes { get; set; }
     }
 }
